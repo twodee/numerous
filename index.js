371aff018544cd25faf15d8334e30da7df28e540
Chris Johnson <crjjrc@gmail.com>
2021-06-17 05:22:19 -0400

    Prime with first draft.

---
+++ index.js
@@ -0,0 +1,100 @@
+const express = require('express');
+
+const app = express();
+const port = 3030;
+
+let n = 0;
+let currentValue = 0;
+
+app.get('/', (request, response) => {
+  const i = n;
+  n += 1;
+
+  response.send(`
+<html>
+  <head>
+    <title>Numerous</title>
+    <style>
+body {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+input[type="checkbox"] {
+  width: 200px;
+  height: 200px;
+}
+    </style>
+  </head>
+  <body>
+    <input id="toggle" type="checkbox">
+    <script>
+
+const toggle = document.getElementById('toggle');
+toggle.addEventListener('input', () => {
+  toggle.disabled = true;
+  fetch('/toggle/${i}', {
+    method: 'PATCH',
+  }).then(() => {
+    toggle.disabled = false;
+  });
+});
+
+    </script>
+  </body>
+</html>
+  `);
+});
+
+app.patch('/toggle/:i', (request, response) => {
+  const i = parseInt(request.params.i);
+  currentValue = currentValue ^ (1 << i);
+  response.send(':)');
+});
+
+app.get('/c', (request, response) => {
+  response.send({value: currentValue});
+});
+
+app.get('/v', (request, response) => {
+  response.send(`
+<html>
+  <head>
+    <title>Numerous</title>
+    <style>
+body {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 80px;
+  font-family: sans-serif;
+}
+    </style>
+  </head>
+  <body>
+    <span id="value-text">${currentValue}</span>
+    <script>
+
+const valueText = document.getElementById('value-text');
+setInterval(() => {
+  fetch('/c')
+    .then(response => response.json())
+    .then(data => {
+      valueText.innerText = data.value;
+    });
+}, 500);
+
+    </script>
+  </body>
+</html>
+  `);
+});
+
+app.listen(port, () => {
+  console.log(`We're live on port ${port}!`);
+});
