75d1466be5ea16076751d1c5addb3c026cb487ac
Chris Johnson <crjjrc@gmail.com>
2021-06-18 01:00:35 +0000

    Fix bugs, add player and session checks.

--- index.js
+++ index.js
@@ -4,68 +4,120 @@ const app = express();
 const port = 3030;
 
 let n = 0;
+let targetN = 8;
 let currentValue = 0;
+let sessionId = -1;
 
 app.get('/', (request, response) => {
   const i = n;
-  n += 1;
+  if (i < targetN) {
+    n += 1;
+    response.send(`
+  <html>
+    <head>
+      <title>Numerous</title>
+      <style>
+  body {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+  }
 
-  response.send(`
-<html>
-  <head>
-    <title>Numerous</title>
-    <style>
-body {
-  width: 100%;
-  height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin: 0;
-}
+  input[type="checkbox"] {
+    width: 200px;
+    height: 200px;
+  }
+      </style>
+    </head>
+    <body>
+      <input id="toggle" type="checkbox">
+      <script>
 
-input[type="checkbox"] {
-  width: 200px;
-  height: 200px;
-}
-    </style>
-  </head>
-  <body>
-    <input id="toggle" type="checkbox">
-    <script>
-
-const toggle = document.getElementById('toggle');
-toggle.addEventListener('input', () => {
-  toggle.disabled = true;
-  fetch('/toggle/${i}', {
-    method: 'PATCH',
-  }).then(() => {
-    toggle.disabled = false;
+  const toggle = document.getElementById('toggle');
+  toggle.addEventListener('input', () => {
+    toggle.disabled = true;
+    const options = {
+      method: 'PATCH',
+    };
+    fetch('/toggle/${i}/session/${sessionId}', options)
+      .then(response => response.json())
+      .then(data => {
+        if (!data.ok) {
+          alert(data.message);
+        }
+        toggle.disabled = false;
+      });
   });
+
+      </script>
+    </body>
+  </html>
+    `);
+  } else {
+    response.send(`
+  <html>
+    <head>
+      <title>Numerous</title>
+      <style>
+  body {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
+    font-family: sans-serif;
+    font-size: 48px;
+  }
+
+      </style>
+    </head>
+    <body>
+      This round has enough players already.
+    </body>
+  </html>
+    `);
+  }
 });
 
-    </script>
-  </body>
-</html>
-  `);
-});
-
-app.patch('/toggle/:i', (request, response) => {
-  const i = parseInt(request.params.i);
-  currentValue = currentValue ^ (1 << i);
-  response.send(':)');
+app.patch('/toggle/:i/session/:sessionId', (request, response) => {
+  const playerSessionId = parseInt(request.params.sessionId);
+  if (playerSessionId === sessionId) {
+    const i = parseInt(request.params.i);
+    currentValue = currentValue ^ (1 << i);
+    response.send({
+      ok: true,
+      message: ':)',
+    });
+  } else {
+    response.send({
+      ok: false,
+      message: 'Your round is over.',
+    });
+  }
 });
 
 app.get('/c', (request, response) => {
-  if (n < 8) {
-    response.send({value: `Players: ${n}`});
+  if (n < targetN) {
+    response.send({value: `Players: ${n}/${targetN}`});
   } else {
     response.send({value: currentValue});
   }
 });
 
-app.get('/v', (request, response) => {
+app.get('/v/:target', (request, response) => {
+  const oldSessionId = sessionId;
+  do {
+    sessionId = Math.floor(Math.random() * 10000000);
+  } while (sessionId === oldSessionId);
+
+  targetN = parseInt(request.params.target);
   n = 0;
+  currentValue = 0;
+
   response.send(`
 <html>
   <head>
@@ -84,7 +136,7 @@ body {
     </style>
   </head>
   <body>
-    <span id="value-text">${currentValue}</span>
+    <span id="value-text"></span>
     <script>
 
 const valueText = document.getElementById('value-text');
